402fc0295010c5c0069dfb3cd9b2c8f94f28d0bd
shivamdattapurkayastha99 <67202581+shivamdattapurkayastha99@users.noreply.github.com>
2022-03-04 22:40:02 +0530

    first commit

--- project/story/models.py
+++ project/story/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 class Category(models.Model):
     name=models.CharField(max_length=150,db_index=True,)
@@ -8,6 +8,8 @@ class Category(models.Model):
         ordering=('-name',)
     def __str__(self):
         return self.name
+    def get_absolute_url(self):
+        return reverse('story:story_category')
 class Story(models.Model):
     category=models.ForeignKey(Category,on_delete=models.CASCADE)
     title=models.CharField(max_length=150)
